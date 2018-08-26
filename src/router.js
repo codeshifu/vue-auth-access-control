880e83ea8af2fe16feaf973f5ded168fa38756d5
Olushi Luqman O <olushilukmon03@gmail.com>
2018-08-26 11:22:36 +0100

    updated router

--- src/router.js
+++ src/router.js
@@ -1,24 +1,35 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import About from './views/About.vue';
 
 Vue.use(Router);
+
+// lazy-loaded routes
+const Login = () => import('@/components/Login');
+const Register = () => import('@/components/Register');
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 });
