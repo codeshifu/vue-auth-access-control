a80cd05b12ddb10cb8c627fc9520202a7116ac91
Olushi Luqman O <olushilukmon03@gmail.com>
2018-08-26 18:35:08 +0100

    refactor router

--- src/router.js
+++ src/router.js
@@ -2,12 +2,10 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
+import Login from '@/components/Login';
+import Register from '@/components/Register';
 
 Vue.use(Router);
-
-// lazy-loaded routes
-const Login = () => import('@/components/Login');
-const Register = () => import('@/components/Register');
 
 export default new Router({
   routes: [
