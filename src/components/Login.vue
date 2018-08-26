92338f7a10ef3c15d398327d16a958e2c38fefd8
Olushi Luqman O <olushilukmon03@gmail.com>
2018-08-26 11:21:30 +0100

    create login/register components

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <form class="login" @submit.prevent="login">
+            <h1>Sign in</h1>
+            <label>Email</label>
+            <input required v-model="email" type="email" placeholder="Name" />
+            <label>Password</label>
+            <input required v-model="password" type="password" placeholder="Password" />
+            <hr/>
+            <button type="submit">Login</button>
+            <p>Don't have an account?
+                <router-link to='/register'>Register</router-link>
+            </p>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  }
+};
+</script>
