cc63dad6988853144d4adc62f0700029697c9b0f
Olushi Luqman O <olushilukmon03@gmail.com>
2018-08-27 19:40:59 +0100

    manage login state with vuex

--- src/store.js
+++ src/store.js
@@ -1,10 +1,34 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { register, login } from './controllers/authController';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    user: null
+  },
+  getters: {
+    isLoggedIn: state => {
+      return state.user;
+    }
+  },
+  mutations: {
+    LOGIN(state, payload) {
+      state.user = payload.email;
+    }
+  },
+  actions: {
+    login({ commit }, payload) {
+      return new Promise((resolve, reject) => {
+        login(payload)
+          .then(data => {
+            commit('LOGIN', data);
+            window.localStorage.setItem('token', data.token);
+            resolve(data);
+          })
+          .catch(err => reject(err));
+      });
+    }
+  }
 });
